0114d9a6d2c9e9f2dec969c5fc3ff7179cb62f7c
Steven Yang <stevenwaterloo2015@gmail.com>
2023-06-27 21:52:48 -0400

    remove key

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "3bCtUckjLIzo1OwrWZPYdRQrdNdvRLT8WhlG06a5"
+    const val API_KEY = ""
 }
